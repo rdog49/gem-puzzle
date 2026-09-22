40fa6a283123dddfb994e10c3e4a9129bfa01cee
VladimirSA <vladimisav@gmail.com>
2026-09-22 12:45:19 +0600

    feat: create grid data structure for 4x4 board & minor fixes for webpack

--- src/logic.js
+++ src/logic.js
@@ -1,12 +1,20 @@
-function newGameTemp() {
-  let arr = [];
-  for (i = 0; i < 4; ++i) {
-    arr[i] = [];
-    for (j = 0; j < 4; ++j) {
-      if (i + j != 6)
-        arr[i][j] = i * 4 + j + 1;
-      else
-        arr[i][j] = "";
+// Координаты пустой ячейки (по умолчанию правый нижний угол)
+let emptyRow = 3;
+let emptyCol = 3;
+
+// базовый массиив
+function createInitialBoard() {
+  const board = [];
+  let counter = 1;
+  for (let i = 0; i < 4; i++) {
+    board[i] = [];
+    for (let j = 0; j < 4; j++) {
+      if (i === 3 && j === 3) {
+        board[i][j] = ''; 
+      } else {
+        board[i][j] = counter++;
+      }
     }
   }
+  return board;
 }
